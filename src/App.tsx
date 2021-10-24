6d688c4d548a9c678d927d26cbb7495aeddd72d4
Joshua Richardson <flayedflesh@me.com>
2021-10-24 07:02:02 -0700

    Fixed app.tsx to point to home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// react
+import React from 'react'
 
-function App() {
+// modules
+import { BrowserRouter as Router, Switch } from 'react-router-dom'
+
+// project files
+import { HomePage } from 'pages'
+import { ProtectedRoute } from 'components'
+import Theme from 'styles/ThemeProvider'
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Theme>
+      <Router>
+        <Switch>
+          <ProtectedRoute exact path='/' component={HomePage} />
+        </Switch>
+      </Router>
+    </Theme>
+  )
 }
-
-export default App;
